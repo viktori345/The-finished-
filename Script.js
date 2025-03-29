915090a14b9101a1f3c198bde66053eae28f334e
viktori345 <kirusinaviktoria3@gmail.com>
2025-03-29 17:36:15 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,8 @@
+document.querySelector('.cta-button').addEventListener('mouseover', function() {
+    this.style.transform = 'scale(1.05)';
+});
+
+document.querySelector('.cta-button').addEventListener('mouseout', function() {
+    this.style.transform = 'scale(1)';
+});
+
