19e57e224f78d08f557569c0fc9a790d2a2483c2
viktori345 <kirusinaviktoria3@gmail.com>
2025-04-05 16:55:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('.cta-button').addEventListener('mouseover', function() {
+    this.style.transform = 'scale(1.05)';
+});
+
+document.querySelector('.cta-button').addEventListener('mouseout', function() {
+     this.style.transform = 'scale(1)';
+ });
+
+ $('.back-to-top').click(function () {
+    $('body,html').animate({ scrollTop: 0}, 800); 
+});
+
+
